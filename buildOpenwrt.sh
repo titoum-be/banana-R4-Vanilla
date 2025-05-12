854d6492c790678ff838ffccf59937feb5076a5a
titoum-be <titoum@gmail.com>
2025-05-12 10:13:03 +0200

    Update buildOpenwrt.sh
    update script

--- buildOpenwrt.sh
+++ buildOpenwrt.sh
@@ -1,8 +1,14 @@
+rm -rf openwrt
+
 git clone https://git.openwrt.org/openwrt/openwrt.git
 
 git checkout openwrt-24.10
+#git checkout 0b392b925fa16c40dccc487753a4412bd054cd63
+
+cd openwrt
 
 umask 022
+
 git clone --branch master --single-branch --no-tags --recurse-submodules https://github.com/fantastic-packages/packages.git fantastic_packages
 cd fantastic_packages
 for v in master 22.03 23.05 24.10; do
@@ -23,4 +29,9 @@ EOF
 
 umask 022
 git clone --depth 1 --branch master --single-branch --no-tags --recurse-submodules https://github.com/fantastic-packages/packages package/fantastic_packages
+
+cp ../myConfig.12052025 myConfig.12052025
 make menuconfig
+
+make -j$(nproc)
+cp  openwrt/bin/targets/mediatek/filogic/openwrt-mediatek-filogic-bananapi_bpi-r4-sdcard.img.gz openwrt-bananapi_r4_fantastic-sdcard_$(date +"%Y_%m_%d_%I_%M_%p").img.gz
